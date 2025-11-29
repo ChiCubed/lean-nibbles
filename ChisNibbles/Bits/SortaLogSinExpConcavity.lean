ef8056b2402a02d1d4557b1a5f362d35722dedc2
Albert Smith <chicbrt@gmail.com>
2025-11-29 17:26:17 +0800

    add a new proof of sin(x)/sin(rx)

--- ChisNibbles/Bits/SortaLogSinExpConcavity.lean
+++ ChisNibbles/Bits/SortaLogSinExpConcavity.lean
@@ -2,6 +2,62 @@ import Mathlib
 
 open Set Real
 
+
+
+namespace Short
+variable {r : ℝ} (hr : 1 < r)
+include hr
+
+attribute [grind! .] mul_pos div_pos inv_pos sin_pos_of_mem_Ioo cos_pos_of_mem_Ioo tan_pos_of_pos_of_lt_pi_div_two
+@[bound, grind →] lemma pi_div_lt : π / r < π := by bound
+@[grind =, grind =_] lemma mul_lt_pi {x : ℝ} : x < π / r ↔ r * x < π := by rw [← lt_div_iff₀']; cutsat
+@[bound, grind! .] lemma sin_pos_r {x : ℝ} (hx : x ∈ Ioo 0 (π / r)) : 0 < sin (r * x) := by grind
+
+attribute [fun_prop]
+  HasDerivAt
+  hasDerivAt_id hasDerivAt_const HasDerivAt.comp HasDerivAt.prodMk
+  HasDerivAt.add HasDerivAt.fun_add HasDerivAt.sub HasDerivAt.fun_sub
+  HasDerivAt.mul HasDerivAt.fun_mul HasDerivAt.div HasDerivAt.fun_div
+  hasDerivAt_sin hasDerivAt_cos
+theorem blob : StrictMonoOn (fun x => sin x / sin (r * x)) (Ico 0 (π / r)) := by
+  set f := fun x => sin x / sin (r * x)
+  rw [← Ioo_insert_left (by bound), strictMonoOn_insert_iff_of_forall_ge (by grind)]
+  refine ⟨by simp [f]; grind, ?_⟩
+
+  -- due to some kind of fun_prop bug, disch := grind isn't working, so we do this instead
+  have : ∀ x ∈ Ioo 0 (π / r), sin (r * x) ≠ 0 := by grind
+  let g x := cos x * sin (r * x) - r * sin x * cos (r * x)
+  have hf' x (hx : x ∈ Ioo 0 (π / r) := by grind) : HasDerivAt f (g x / sin (r * x) ^ 2) x := by
+    have : HasDerivAt f _ x
+    . fun_prop (disch := solve_by_elim)
+    convert this using 1; ring
+  apply strictMonoOn_of_deriv_pos (convex_Ioo ..) (by fun_prop (disch := solve_by_elim))
+  rw [interior_Ioo]
+  intro x hx
+  rw [hf' x |>.deriv]; clear * - hr hx
+  field_simp [sin_pos_r hr hx]
+  rw [zero_mul]
+
+  suffices StrictMonoOn (fun x => g x) (Ico 0 (π / r)) by
+    convert this (a := 0) (b := x) (by grind) (by grind) (by grind); simp [g]
+  have hg' x : HasDerivAt g ((r ^ 2 - 1) * sin x * sin (r * x)) x := by
+    have : HasDerivAt g _ x
+    . fun_prop
+    convert this using 1; ring
+  apply strictMonoOn_of_deriv_pos (convex_Ico ..) (by fun_prop)
+  rw [interior_Ico]
+  intro x hx
+  rw [hg' x |>.deriv]; clear * - hr hx
+
+  have : 0 < r ^ 2 - 1 := by nlinarith
+  have : 0 < sin x := by grind
+  have : 0 < sin (r * x) := by grind
+  positivity
+end Short
+
+
+
+namespace Long
 lemma Real.tan_strictConvexOn : StrictConvexOn ℝ (Ico 0 (π / 2)) tan := by
   apply strictConvexOn_of_deriv2_pos (convex_Ico ..) (continuousOn_tan_Ioo.mono (by grind))
   rw [interior_Ico]
@@ -93,3 +149,4 @@ theorem bob : StrictMonoOn (fun x => sin x / sin (r * x)) (Ico 0 (π / r)) := by
     suffices cot x ≤ 0 by nlinarith [show 0 < x from hx.1.trans_lt' (by bound)]
     convert cot_anti.antitoneOn (a := π / 2) (by grind) hx hx.1
     simp [cot_eq_cos_div_sin]
+end Long
